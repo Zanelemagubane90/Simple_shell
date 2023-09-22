ed7c7f90de13fefdbc6dcbe025df99e23c2959e8
Zanele <122941010+Zanelemagubane90@users.noreply.github.com>
2023-09-22 16:39:04 -0400

    Update main.c

--- main.c
+++ main.c
@@ -10,53 +10,36 @@
 
 int main(int ac, char **av)
 {
-    // Initialize the info structure with INFO_INIT
-    info_t info = INFO_INIT;
+	info_t infor[] = {INFO_INIT};
+	int f_x = duo;
 
-    // Default file descriptor value (if not specified)
-    int prfx = -1;
+	asm("mov %1, %0\n\t"
+		"add $3, %0"
+		: "=r"(f_x)
+		: "r"(f_x));
 
-    // Assembly code snippet (not clear without context)
-    asm("mov %1, %0\n\t"
-        "add $3, %0"
-        : "=r"(prfx)
-        : "r"(prfx));
-
-    // Check if there's exactly one command-line argument
-    if (ac == 2)
-    {
-        // Try to open the file specified by the command-line argument
-        prfx = open(av[1], O_RDONLY);
-        if (prfx == -1)
-        {
-            // Handle file open errors
-            if (errno == EACCES)
-                exit(126);
-            if (errno == ENOENT)
-            {
-                _eputs(av[0]);
-                _eputs(": 0: Can't open ");
-                _eputs(av[1]);
-                _eputchar('\n');
-                _eputchar(BUF_FLUSH);
-                exit(127);
-            }
-            return EXIT_FAILURE;
-        }
-
-        // Set the read file descriptor in the info structure
-        info.readfd = prfx;
-    }
-
-    // Populate the environment list
-    populate_env_list(&info);
-
-    // Read command history from a file
-    read_history(&info);
-
-    // Start the shell
-    hsh(&info, av);
-
-    // Return success status
-    return EXIT_SUCCESS;
+	if (ac == duo)
+	{
+		f_x = open(av[uno], O_RDONLY);
+		if (f_x == n_uno)
+		{
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				_eputs(av[none]);
+				_eputs(": 0: Can't open ");
+				_eputs(av[uno]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
+		}
+		infor->readfd = f_x;
+	}
+	populate_env_list(infor);
+	read_history(infor);
+	hsh(infor, av);
+	return (EXIT_SUCCESS);
 }
