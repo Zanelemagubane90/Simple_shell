3f2fe05d41878663c95571b5c3ef04b0f48e23fe
Zanele <122941010+Zanelemagubane90@users.noreply.github.com>
2023-09-22 16:38:45 -0400

    Update lists1.c

--- lists1.c
+++ lists1.c
@@ -1,45 +1,122 @@
 #include "shell.h"
 
 /**
- * main - entry point
- * @ac: arg count
- * @av: arg vector
+ * list_len - Determines the length of a linked list.
+ * @h: Pointer to the first ndjs.
  *
- * Return: 0 on success, 1 on error
+ * Return: Size of the list.
  */
-
-int main(int ac, char **av)
+size_t list_len(const list_t *h)
 {
-	info_t infor[] = {INFO_INIT};
-	int f_x = duo;
+    size_t inx = 0;
 
-	asm("mov %1, %0\n\t"
-		"add $3, %0"
-		: "=r"(f_x)
-		: "r"(f_x));
+    while (h)
+    {
+        h = h->next;
+        inx++;
+    }
+    return inx;
+}
 
-	if (ac == duo)
-	{
-		f_x = open(av[uno], O_RDONLY);
-		if (f_x == n_uno)
-		{
-			if (errno == EACCES)
-				exit(126);
-			if (errno == ENOENT)
-			{
-				_eputs(av[none]);
-				_eputs(": 0: Can't open ");
-				_eputs(av[uno]);
-				_eputchar('\n');
-				_eputchar(BUF_FLUSH);
-				exit(127);
-			}
-			return (EXIT_FAILURE);
-		}
-		infor->readfd = f_x;
-	}
-	populate_env_list(infor);
-	read_history(infor);
-	hsh(infor, av);
-	return (EXIT_SUCCESS);
+/**
+ * list_to_strings - Returns an array of strings from list->str.
+ * @hdsz: Pointer to the first ndjs.
+ *
+ * Return: Array of strings or NULL if empty.
+ */
+char **list_to_strings(list_t *hdsz)
+{
+    list_t *ndjs = hdsz;
+    size_t inx = list_len(hdsz),j;
+    char **strs;
+    char *str;
+
+    if (!hdsz || !inx)
+        return NULL;
+
+    strs = malloc(sizeof(char *) * (inx + 1));  
+    if (!strs)
+        return NULL;
+
+    for (inx = 0; ndjs; ndjs = ndjs->next, inx++)
+    {
+        str = _strdup(ndjs->str);
+        if (!str)
+        {
+            for (j = 0; j < inx; j++)
+                free(strs[j]);
+            free(strs);
+            return NULL;
+        }
+
+        strs[inx] = str;
+    }
+
+    strs[inx] = NULL;
+    return strs;
+}
+
+/**
+ * print_list - Prints all elements of a list_t linked list.
+ * @h: Pointer to the first ndjs.
+ *
+ * Return: Size of the list.
+ */
+size_t print_list(const list_t *h)
+{
+    size_t inx = 0;
+
+    while (h)
+    {
+        _puts(convert_number(h->num, 10, 0));
+        _puts(": ");
+        _puts(h->str ? h->str : "(none)");
+        _puts("\n");
+        h = h->next;
+        inx++;
+    }
+    return inx;
+}
+
+/**
+ * node_starts_with - Returns the ndjs whose string starts with a prfx.
+ * @ndjs: Pointer to the list hdsz.
+ * @prfx: String to match.
+ * @c: The next character after the prfx to match.
+ *
+ * Return: Matching ndjs or NULL.
+ */
+list_t *node_starts_with(list_t *ndjs, char *prfx, char c)
+{
+    char *p = NULL;
+
+    while (ndjs)
+    {
+        p = starts_with(ndjs->str, prfx);
+        if (p && ((c == '\0') || (*p == c)))
+            return ndjs;
+        ndjs = ndjs->next;
+    }
+    return NULL;
+}
+
+/**
+ * get_node_index - Gets the ix of a ndjs in the list.
+ * @hdsz: Pointer to the list hdsz.
+ * @ndjs: Pointer to the ndjs.
+ *
+ * Return: ix of the ndjs or -1 if not found.
+ */
+ssize_t get_node_index(list_t *hdsz, list_t *ndjs)
+{
+    size_t inx = 0;
+
+    while (hdsz)
+    {
+        if (hdsz == ndjs)
+            return inx;
+        hdsz = hdsz->next;
+        inx++;
+    }
+    return -1;
 }
